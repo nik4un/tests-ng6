e1e6312228c1f6109a8da4242108feac76342a4f
nik4un <nik4un@yandex.ru>
2018-07-04 11:07:40 +0300

    [2] Two-way Data Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { WarningComponent } from './warning/warning.component';
-import { SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WarningComponent,
-    SuccessComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
